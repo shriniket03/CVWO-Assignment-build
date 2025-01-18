d5a9335c8899b30a0fe77c6f79f988cb4a8df37e
Shriniket <shriniket03@gmail.com>
2025-01-18 19:23:56 +0800

    Modify env File Path

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,11 +5,25 @@ import (
 	"log"
 	"net/http"
 	"github.com/shriniket03/CRUD/backend/internal/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := router.Setup()
+	port := GoDotEnvVariable("PORT")
 	fmt.Print("Listening on port 8000 at http://localhost:8000!")
-
 	log.Fatalln(http.ListenAndServe(":8000", r))
+}
+
+func GoDotEnvVariable(key string) string {
+	// load .env file
+	err := godotenv.Load("../../.env")
+	if err != nil {
+	  log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+type Database struct {
+	Ref *sql.DB
 }
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -12,7 +12,7 @@ import (
 
 func GoDotEnvVariable(key string) string {
 	// load .env file
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../../.env")
 	if err != nil {
 	  log.Fatalf("Error loading .env file")
 	}
